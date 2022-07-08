db654d5238a975a5bff0a644d0c50e0b643fb7f6
pascalloria <dev.pascalloria@gmail.com>
2022-07-08 22:36:48 +0200

    Ajout d'un background à la navbar

--- src/Home.js
+++ src/Home.js
@@ -11,6 +11,19 @@ const Home = () =>{
     
     return (
         <div className="home">
+            <div className="intro">
+                <p> Bienvenue à toi</p>
+                <br />
+                <p> Es-tu un seigneur injustement harcelé au porte de ton chateau par des citoyens ingrats ?</p>
+                <br />
+                <p> Grace à ce guide, a toi la joie de voir ces hordes de va-nue-pied balayer par ton armeé.
+                Les cadavres de ces imbéciles joncheront les imposantes fortification de ta cité.</p>
+            </div>
+            <br />
+            <p> Sur ce site tu trouveras des builds Orders et conseil pour réussir chaque mission de la campagne</p>
+
+            <br /><br />
+
             {isPending && <div> Loadding ...</div>}
             {error && <div> {error}</div>}
             {articles && <ArtcileList articles={articles} />}
