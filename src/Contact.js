252fb2632633fec45d9ec21f1484cc297bd67bce
pascalloria <dev.pascalloria@gmail.com>
2022-07-10 14:41:30 +0200

    Modification de la page contact

--- src/Contact.js
+++ src/Contact.js
@@ -6,8 +6,14 @@ const Contact = () => {
         <div className="contact">
             
             <h2> Contact</h2>
-            <br />
-            <p> Pour me contacter :<a href="mailto:dev.pascalloria@gmail.com">dev.pascalloria@gmail.com</a></p>            
+            <br />    
+            <p> Si vous souhaitez partager une stratégie ou un BO pour une mission merci de me contacter par mail</p>
+
+            <br /><br />
+            <p> Pour me contacter :<a href="mailto:dev.pascalloria@gmail.com">dev.pascalloria@gmail.com</a></p>  
+            <br /><br />         
+            <p> Ce site est construit grace à REACT 1.8</p>
+        
         </div>
      );
 }
